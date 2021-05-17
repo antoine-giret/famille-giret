1e97ac90c94a590d0b1d953b955a9f78a735d00e
Antoine Giret <ant.giret@gmail.com>
2021-05-17 23:04:46 +0200

    chore: add eslint and prettier

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+function NotFound(): JSX.Element {
+  return (
+    <main>
+      <title>404</title>
+      <h1>404 Not Found</h1>
+    </main>
+  );
+}
+
+export default NotFound;
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+function Index(): JSX.Element {
+  return (
+    <main>
+      <title>Index</title>
+      <h1>Hello World</h1>
+    </main>
+  );
+}
+
+export default Index;
