efa4f6d1e09e9afd7498854db3fd6ca217335474
Antoine Giret <ant.giret@gmail.com>
2021-11-14 13:01:37 +0100

    fix: change language when navigating to french home

--- src/pages/index.fr.tsx
+++ src/pages/index.fr.tsx
@@ -1,9 +1,16 @@
 import { HLocation } from '@reach/router';
 import * as React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import Index from '../templates/index';
 
 function IndexFr({ location }: { location: HLocation }): JSX.Element {
+  const { i18n } = useTranslation();
+
+  React.useEffect(() => {
+    i18n.changeLanguage('fr');
+  }, []);
+
   return <Index location={location} />;
 }
 
